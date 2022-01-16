f47aa09fb6cf56b0de00ff157bbd5204a9b282bb
muhammedsheikhh <mosheikh78690@gmail.com>
2022-01-15 18:10:19 -0600

    frontend is created

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,6 +24,7 @@ class ProductsController < ApplicationController
     product.name = params[:name] || product.name
     product.price = params[:price] || product.price
     product.description = params[:description] || product.description
+    product.image_url = params[:image_url] || product.image_url
     product.save
     render json: product
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Product.create(name: "Mercedes-benz Class A", price: "42000", description: "brand new Merceded-benz Class A with navigation, led lights and panoramic roof")
+Product.create(name: "Mercedes-benz Class C", price: "55000", description: "brand new Merceded-benz Class C with navigation, led lights and panoramic roof")
+Product.create(name: "Mercedes-benz Class E", price: "810000", description: "brand new Merceded-benz Class E with navigation, led lights and panoramic roof")
+Product.create(name: "Mercedes-benz Class S", price: "110000", description: "brand new Merceded-benz Class S with navigation, led lights and panoramic roof")
+Product.create(name: "Mercedes-benz Class G", price: "240000", description: "brand new Merceded-benz Class G with navigation, led lights and panoramic roof")
