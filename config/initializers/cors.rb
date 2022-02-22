7710d35b4ba1daad95eaa4b2f553aca7a6a4ec78
muhammedsheikhh <mosheikh78690@gmail.com>
2022-02-21 20:35:23 -0600

    commit message

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "mini-capstone.peterxjang.com", "localhost:8080"
+    origins "mini-capstone.peterxjang.com", "localhost:8080", "stoic-payne-f85ae5.netlify.app"
 
     resource "*",
       headers: :any,
